2d1f7262d2af18af21eac1be76f84d60071e6ff9
Serg <gavelyuk.sergey.13@gmail.com>
2017-04-14 20:12:43 +0300

    upd

--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var http = require('http');
-var html = '\n<!doctype>\n    <html>\n        <head>\n            <meta charset ="utf-8">\n            <title>Node.js Basics</title>\n            <limk rel ="stylesheet" href="app.css">\n        </head>\n        <body>\n            <h1>Node.js Basics</h1>\n            <button>Tap on me</button>\n            \n            <script src="app.js"></script>\n        </body>\n</html>\n';
+var html = '\n<!doctype>\n    <html>\n        <head>\n            <meta charset ="utf-8">\n            <title>Node.js Basics</title>\n            <link rel ="stylesheet" href="app.css">\n        </head>\n        <body>\n            <h1>Node.js Basics</h1>\n            <button>Tap on me</button>\n            \n            <script src="app.js"></script>\n        </body>\n</html>\n';
 var css = '\n body{\n    margin:0;\n    padding:0;\n    text-align:center;\n}\nh1{\n    background-color: #43853d;\n    color:white;\n    padding:.5em;\n    font-familt:\'Consolas\'\n}\n';
 var js = '\n    const button = document.querySelector(\'button\');\n    button.addEventListener(\'click\', event => alert(\'Node.js in action\'));\n';
 
