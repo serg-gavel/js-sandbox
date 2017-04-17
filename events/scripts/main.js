ac6af72403e2b1b7dbd65b0c6fc52c1cf8e12563
Serg <gavelyuk.sergey.13@gmail.com>
2017-04-17 19:55:33 +0300

    upd
    upd and fix

--- events/scripts/main.js
+++ events/scripts/main.js
@@ -1,18 +1,27 @@
-var simon = document.getElementById('simon');
-var bruce = document.getElementById('bruce');
-var ben = document.getElementById('ben');
+var simon = document.getElementById("simon");
+var bruce = document.getElementById("bruce");
+var ben = document.getElementById("ben");
 
+simon.addEventListener("click", picLink);
+bruce.addEventListener("click", picLink);
+ben.addEventListener("click", picLink);
 
-simon.addEventListener('click',picLink);
-bruce.addEventListener('click',picLink);
-ben.addEventListener('click',picLink);
 function picLink() {
-    var picId = this.attributes['data-img'].value;
+    var allImages = document.querySelectorAll("img");
+    var picId = this.attributes["data-img"].value;
     var pic = document.getElementById(picId);
-    if(pic.className === 'hide'){
-        pic.className = '';
+
+    for (var i = 0; i < allImages.length; i++) {
+        if (allImages[i] !== pic) {
+            allImages[i].className = "hide";
+        }
     }
-    else{
-        pic.className='hide';
+
+
+
+    if (pic.className === "hide") {
+        pic.className = "";
+    } else {
+        pic.className = "hide";
     }
 }
