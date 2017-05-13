e7dbcec621fb5dfb0ff23e89659d25e8a197568a
Serg <gavelyuk.sergey.13@gmail.com>
2017-05-14 02:15:35 +0300

    upd index.html and classes.js

--- babel/src/classes.js
+++ babel/src/classes.js
@@ -2,11 +2,23 @@ class Task {
 
     constructor(title = ''){
         this.title = title;
+        this.done = false;
+        Task.count += 1;
         console.log('Создание задачи');
     }
 
+    complete(){
+        this.done = true;
+        console.log(`Задача "${this.title}" выполнена`);
+    }
 }
 
+Task.count = 0;
+
 let task = new Task('Жигульнуть пивца');
+let task2 = new Task('Пивнуть жигульца');
 
 console.log(task.title);
+console.log(task2.title);
+console.log(Task.count);
+task2.complete();
